91085bdb51fdef84f6926cb3a09189cff7a2470e
Victor <victor.mkrt@gmail.com>
2026-02-15 16:42:11 +0400

    optimize the functions

--- main.js
+++ main.js
@@ -4,12 +4,8 @@ const button = document.querySelector("button");
 const body = document.querySelector("body")
 
 function getLettersCount(text) {
-    console.log("function changeLettersCount is called!");
-
-    let letters = 0;
-    console.log(text.split(""));
-    
-    text.split("").map((item, i) => {
+    let letters = 0;    
+    text.split("").forEach((item) => {
         if ((item >= 'A' && item <= 'Z') || (item >= 'a' && item <= 'z')) {
             letters++;
         }
@@ -18,45 +14,35 @@ function getLettersCount(text) {
 }
 
 function getWordsCount(text) {
-    console.log("function changeWordsCount is called!");
-
     let words = 0;
-    text.split("").map((item, i, text) => {
-        if (item == ' ' || item == '\n') {
-            if ((text[i + 1] >= 'A' && text[i + 1] <= 'Z') || (text[i + 1] >= 'a' && text[i + 1] <= 'z')) {
-                words++;
-            }
-        }
+    text.split(/\s+/).forEach((item) => {
+        if (item) words++;
     });
     return words;
 }
 
 function getSentencesCount(text) {
-    console.log("function changeSentencesCount is called!");
-
     let sentences = 0;
-    text.split("").map((item, i, text) => {
-        if (item == '.' || item == '\n' || item == ',') {
-            if ((text[i + 1] >= 'A' && text[i + 1] <= 'Z') || (text[i + 1] >= 'a' && text[i + 1] <= 'z') || (text[i + 2] >= 'A' && text[i + 2] <= 'Z') || (text[i + 2] >= 'a' && text[i + 2] <= 'z')) {
-                sentences++;
-            }
-        }
+    text.split(/[.!?]+/).forEach((item) => {
+        if (item.trim().length > 0) sentences++;
     });
     return sentences;
 }
 
 input.addEventListener("keyup", (event) => {
     const counterChildrens = counter.children;
+    
     counterChildrens[0].textContent = "Letters: " + getLettersCount(event.target.value);
     counterChildrens[1].textContent = "Words: " + getWordsCount(event.target.value);
     counterChildrens[2].textContent = "Sentences: " + getSentencesCount(event.target.value);
 });
 
-const colors = ["#456123", "#ae3243", "#ff3276", "#432fae", "#7643ff"];
-const backgroundColor = ["#23350dff", "#69111dff", "#90123cff", "#1f0f75ff", "#471ac4ff"];
-
 button.addEventListener("click", (event) => {
+    const colors = ["#456123", "#ae3243", "#ff3276", "#432fae", "#7643ff"];
+    const backgroundColor = ["#192709ff", "#450911ff", "#590723ff", "#100647ff", "#260c70ff"];
     let randomIndex = Math.floor(Math.random() * 10) % 5;
-    input.setAttribute("style", `background-color: ${colors[randomIndex]}`);
+    input.setAttribute("style", `background-color: ${colors[randomIndex]}; color: ${backgroundColor[randomIndex]}`);
     body.setAttribute("style", `background-color: ${backgroundColor[randomIndex]}`);
+    button.setAttribute("style", `background-color: ${colors[randomIndex]}; color: ${backgroundColor[randomIndex]}`);
+    counter.children[0].setAttribute("style", `background-color: ${backgroundColor[randomIndex]}`);
 });
